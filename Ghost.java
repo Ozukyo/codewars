c248b63c6792daf266a79b0b2219be98f7e9a085
Ozukyo <ozukyo@gmail.com>
2020-01-09 20:51:56 +0100

    add getColor function and list of colors

--- Ghost.java
+++ Ghost.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Ghost {
-    
+    List<String> colors = Arrays.asList("white", "yellow", "purple", "red");
+    public static void main(String[] args) {
+       
+    } 
+
+    public String getColor() {
+        Random randomColor = new Random();
+        String color = colors.get(randomColor.nextInt(colors.size()));
+
+        return color;
+    }
+
 }
