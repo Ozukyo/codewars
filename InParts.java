d22dae3a32e992e39f8dda3bb2fd276f0f44c0f2
Ozukyo <ozukyo@gmail.com>
2020-01-10 12:55:48 +0100

    final version of excersise

--- InParts.java
+++ InParts.java
@@ -1,3 +1,22 @@
 public class InParts {
-    
+    public static void main(String[] args) {
+        System.out.println(splitInParts("supercalifragilisticexpialidocious", 3));
+
+    }
+
+
+    public static String splitInParts(String s, int partLenght) { 
+        String stringOfLetters= "";
+        int index = 0;
+
+
+        while(index<s.length()) {
+            stringOfLetters += " " + (s.substring(index, Math.min(index + partLenght, s.length())));
+            index += partLenght;
+        }
+        return stringOfLetters.trim();
+        
+        
+    }
+   
 }
